2647872db50d77500787688b86e4b83375e7395f
haon7663 <89632611+haon7663@users.noreply.github.com>
2024-07-25 09:51:19 +0900

    플러그인 추가 (닷트윈, DrawIf., PlayModeSetting)

--- Assets/Scripts/PatternSO.cs
+++ Assets/Scripts/PatternSO.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EmissionType { Angle, Entire, }
+public enum FireType { Immediate, Sequence }
+
 [CreateAssetMenu(menuName = "ScriptableObject/PatternSO", fileName = "PatternSO")]
 public class PatternSO : ScriptableObject
 {
     public Projectile projectilePrefab;
 
+    public EmissionType emissionType;
+    public FireType fireType;
+    
     public float duration;
 }
