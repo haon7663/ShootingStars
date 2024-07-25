cedfc3552c900b4b41eca99b78c4280cef0d3a32
ski12 <79460738+ski12@users.noreply.github.com>
2024-07-25 13:29:11 +0900

    플레이어 머리 위 UI

--- Assets/Scripts/HeadUI.cs
+++ Assets/Scripts/HeadUI.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class HeadUI : MonoBehaviour
 {
+    [SerializeField] GameObject pl1HeadUIPrefab, pl2HeadUIPrefab, canvas;
+    [SerializeField] GameObject PL1, PL2;
+    GameObject pl1HeadUI, pl2HeadUI;
+    Camera m_camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_camera = Camera.main;
+        pl1HeadUI = Instantiate(pl1HeadUIPrefab, PL1.transform.position,Quaternion.identity,canvas.transform);
+        pl2HeadUI = Instantiate(pl2HeadUIPrefab, PL2.transform.position, Quaternion.identity, canvas.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pl1HeadUI.transform.position = m_camera.WorldToScreenPoint(PL1.transform.position + new Vector3(0,1.2f,0));
+        pl2HeadUI.transform.position = m_camera.WorldToScreenPoint(PL2.transform.position + new Vector3(0, 1.2f, 0));
     }
 }
