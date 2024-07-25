5d8bf7e431ad8b61ac2a8c626f534a45543c0de7
haon7663 <89632611+haon7663@users.noreply.github.com>
2024-07-25 12:45:30 +0900

    부메랑 투사체 구현

---
+++ Assets/Scripts/BoomerangProjectile.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BoomerangProjectile : Projectile
+{
+    private Rigidbody2D _rigid;
+
+    [SerializeField] private float slowSpeed;
+    private Vector2 _angleVec, _saveAngleVec;
+    private float _t;
+    private void Awake()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+    
+    public override void Initialize(Vector2 startVec, Vector2 targetVec, Health owner)
+    {
+        OwnerHealth = owner;
+        
+        transform.position = startVec;
+        _angleVec = targetVec * moveSpeed;
+        _saveAngleVec = _angleVec;
+    }
+
+    private void Update()
+    {
+        _t += Time.deltaTime * slowSpeed;
+        _rigid.velocity = _angleVec - _saveAngleVec * _t;
+    }
+}
